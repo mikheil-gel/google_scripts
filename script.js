9ab82a4898f513d42541a71575140743a1828240
mikheil-gel <mikheilgelenidze@gmail.com>
2022-05-11 20:22:45 +0400

    improve script

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function onEdit(e) {
-  var app = SpreadsheetApp;
-  var activeSheet = app.getActiveSpreadsheet().getActiveSheet();
-  var row = e.range.getRow();
   var col = e.range.getColumn();
   if (col === 1) {
-    activeSheet.getRange(row, 2).setValue(e.user + ' ' + new Date());
+    var row = e.range.getRow();
+    e.source
+      .getActiveSheet()
+      .getRange(row, 2)
+      .setValue(e.user + ' ' + new Date());
   }
 }
