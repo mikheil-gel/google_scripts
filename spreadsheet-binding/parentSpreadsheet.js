3d68f061829d309fd5ddeeff5e60b40d4e33bc64
mikheil-gel <mikheilgelenidze@gmail.com>
2022-06-17 17:12:52 +0400

    add on change trigger and formatting methods

--- spreadsheet-binding/parentSpreadsheet.js
+++ spreadsheet-binding/parentSpreadsheet.js
@@ -1,12 +1,24 @@
-// custom trigger function
 // !!!
-// should be added manually:
-// Apps Scipt > Triggers (clock icon) > Add Trigger
-// > Choose which function to run: editEvent, Select event type: On edit > Save
-// pop up will show up for granting permissions
-function editEvent(e) {
+// this function should run manually from the Apps Script once
+// creates change event trigger for the spreadsheet
+// permissions should be granted
+function createTriggers() {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  // check if trigger already exist
+  const triggers = ScriptApp.getUserTriggers(ss);
+  let triggerExists = false;
+  triggers.forEach((item) => {
+    if (item.getHandlerFunction() === 'changeEvent') triggerExists = true;
+  });
+  // create trigger if it's first run
+  if (!triggerExists) {
+    ScriptApp.newTrigger('changeEvent').forSpreadsheet(ss).onChange().create();
+  }
+}
+
+function changeEvent(e) {
   // !!!
-  // function takes sheet name and target spreadsheet ID
+  // function takes sheet name and target spreadsheet Id
   // more information below
   updateTargetSpreadsheet(e, 'Sheet1', '1qXSJDoqDEewRMRHkwQdLVoptm89zBKuKDbY8ZwKdo5U');
   updateTargetSpreadsheet(e, 'Sheet2', '1kTSOZXLTs7SaqknVjrqH3qlbNUtVdy8WjZ-KOn2LvZE', 'Sheet1');
@@ -16,9 +28,9 @@ function editEvent(e) {
 // takes 4 parameters:
 // event object,
 // local sheet name,
-// target spreadsheet ID,
+// target spreadsheet Id,
 // target sheet name (optional: not needed if it's the same as the local name)
-function updateTargetSpreadsheet(e, sheetName, ID, targetSheetName = sheetName) {
+function updateTargetSpreadsheet(e, sheetName, spreadsheetId, targetSheetName = sheetName) {
   // get edited sheet name
   const activeSheetName = e.source.getActiveSheet().getName();
 
@@ -50,12 +62,27 @@ function updateTargetSpreadsheet(e, sheetName, ID, targetSheetName = sheetName) 
       });
     });
 
+    // get formatting
+    const background = dataRange.getBackgrounds();
+    const fontColor = dataRange.getFontColors();
+    const fontFamily = dataRange.getFontFamilies();
+    const fontLine = dataRange.getFontLines();
+    const fontSize = dataRange.getFontSizes();
+    const fontStyle = dataRange.getFontStyles();
+    const fontWeight = dataRange.getFontWeights();
+    const textStyle = dataRange.getTextStyles();
+    const horAlign = dataRange.getHorizontalAlignments();
+    const vertAlign = dataRange.getVerticalAlignments();
+    const bandings = dataRange.getBandings();
+    const mergedRanges = dataRange.getMergedRanges();
+
     // get target sheet
-    const targetApp = SpreadsheetApp.openById(ID);
+    const targetApp = SpreadsheetApp.openById(spreadsheetId);
     const targetSheet = targetApp.getSheetByName(targetSheetName);
+    const tergetRange = targetSheet.getRange(rangeNotation);
 
     // update target sheet values
-    targetSheet.getRange(rangeNotation).setValues(rangeValues);
+    tergetRange.setValues(rangeValues);
     // check if  the current sheet has formulas
     if (formulasAddresses.length) {
       // overwrite values with formulas in the target sheet
@@ -63,6 +90,43 @@ function updateTargetSpreadsheet(e, sheetName, ID, targetSheetName = sheetName) 
         const formula = sheet.getRange(cor.row, cor.column).getFormula();
         targetSheet.getRange(cor.row, cor.column).setFormula(formula);
       });
+    }
+    //update formatting
+    tergetRange.setBackgrounds(background);
+    tergetRange.setFontColors(fontColor);
+    tergetRange.setFontFamilies(fontFamily);
+    tergetRange.setFontLines(fontLine);
+    tergetRange.setFontSizes(fontSize);
+    tergetRange.setFontStyles(fontStyle);
+    tergetRange.setFontWeights(fontWeight);
+    tergetRange.setTextStyles(textStyle);
+    tergetRange.setHorizontalAlignments(horAlign);
+    tergetRange.setVerticalAlignments(vertAlign);
+
+    for (let i in bandings) {
+      let srcBandA1 = bandings[i].getRange().getA1Notation();
+      let destBandRange = targetSheet.getRange(srcBandA1);
+
+      destBandRange
+        .applyRowBanding()
+        .setFirstRowColor(bandings[i].getFirstRowColor())
+        .setSecondRowColor(bandings[i].getSecondRowColor())
+        .setHeaderRowColor(bandings[i].getHeaderRowColor())
+        .setFooterRowColor(bandings[i].getFooterRowColor());
+    }
+
+    for (let i = 0; i < mergedRanges.length; i++) {
+      targetSheet.getRange(mergedRanges[i].getA1Notation()).merge();
+    }
+
+    for (let i = 1; i <= dataRange.getHeight(); i++) {
+      let width = sheet.getColumnWidth(i);
+      targetSheet.setColumnWidth(i, width);
+    }
+
+    for (let i = 1; i <= dataRange.getWidth(); i++) {
+      let height = sheet.getRowHeight(i);
+      targetSheet.setRowHeight(i, height);
     }
   }
 }
