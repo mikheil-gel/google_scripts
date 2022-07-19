057f296dff830c4385d50b6d81a2c4d88a93fbfc
mikheil-gel <mikheilgelenidze@gmail.com>
2022-07-19 16:09:21 +0400

    create script for target spreadsheet

---
+++ spreadsheet-binding/filterAndCopyInTarget.js
@@ -0,0 +1,186 @@
+// !!!
+// script uses advanced service: Sheets API
+// it should be added manually:
+// Apps Script (Editor)> Services > Google Sheets API > Add;
+
+// !!!
+// 7 values should be provided:
+// localSpreadsheetId, dataSpreadsheetId, dataSheetName, dataHeaderRows, runOnEveryDays, runAtHour, setFilterFromScript
+
+// local spreadsheet id
+const localSpreadsheetId = '1KhBarhr1Owg5OO-TqK3wdBtvN0_30qaVRS_cFtjctnU';
+
+// data spreadsheet id
+const dataSpreadsheetId = '1BeF-2ax7VS7boKktnlt3NI6Y0RYWkwOzaHcjUa9AoMQ';
+
+// data sheet name
+const dataSheetName = 'Sheet1';
+
+// data header rows count
+const dataHeaderRows = 2;
+
+// number of days function should run (1 - once a day, 7 - once a week)
+const runOnEveryDays = 1;
+
+// time (hour) function should run (integers from 0 to 23)
+const runAtHour = 0;
+
+// option to set filters from the script (boolean value)
+const setFilterFromScript = false;
+// !!!
+// if filter should be set from the script (setFilterFromScript is true),
+// array/s should be added in filterArray:
+
+// fillterArray should contain array/s with two values:
+// first value: column's (capital)letter/s;
+// second value: criteria bulider with method
+// example: ['A', SpreadsheetApp.newFilterCriteria().whenTextNotEqualTo('text')]
+// criteria full list can be found on:
+// https://developers.google.com/apps-script/reference/spreadsheet/filter-criteria-builder
+
+const filterArray = [
+  ['C', SpreadsheetApp.newFilterCriteria().whenTextEqualTo('vue')],
+  ['D', SpreadsheetApp.newFilterCriteria().whenNumberGreaterThan(1)],
+];
+
+// !!!
+// this function should run manually from the Apps Script, when:
+// it is first run or runOnEveryDays/runAtHour variables were changed
+// creates time based trigger function
+// copies current data to target sheet
+// while code should work correctly, I faced some unexpected behavior when changing trigger option;
+// would recommend deleting existing trigger manually, before running a function again;
+function createTrigger() {
+  const triggers = ScriptApp.getProjectTriggers();
+  // check if trigger exists
+  let timeTriggerIndex = null;
+  triggers.forEach((item, index) => {
+    if (item.getHandlerFunction() === 'timeBasedEvent') timeTriggerIndex = index;
+  });
+
+  // delete trigger if it exists
+  if (timeTriggerIndex !== null) {
+    ScriptApp.deleteTrigger(triggers[timeTriggerIndex]);
+  }
+
+  // create time based trigger
+  ScriptApp.newTrigger('timeBasedEvent').timeBased().everyDays(runOnEveryDays).atHour(runAtHour).create();
+
+  // copy data to target spreadsheet
+  copyData();
+}
+
+// trgger function runs once in specified (runOnEveryDays) days
+function timeBasedEvent() {
+  copyData();
+}
+
+// function for coping values to target spreadsheet
+function copyData() {
+  // get data sheet
+  const app = SpreadsheetApp.openById(dataSpreadsheetId);
+  const sheet = app.getSheetByName(dataSheetName);
+
+  // get last Row
+  const lastRow = sheet.getLastRow();
+  // get last Column
+  const lastColumn = sheet.getLastColumn();
+  // get range with data
+  const dataRange = sheet.getDataRange();
+
+  // get range address
+  const rangeNotation = dataRange.getA1Notation();
+
+  // get filter Range
+  let filter = sheet.getFilter();
+  let originalCriteria = [];
+
+  // set filter from the script
+  if (setFilterFromScript) {
+    if (!filter) {
+      // create new filter
+      const filterRangeNotation = rangeNotation.replace('A1', `A${dataHeaderRows}`);
+      filter = sheet.getRange(filterRangeNotation).createFilter();
+    } else {
+      for (let i = 1; i <= lastColumn; i++) {
+        if (filter.getColumnFilterCriteria(i)) {
+          // save original filter criteria
+          originalCriteria.push(filter.getColumnFilterCriteria(i).copy());
+          // clear criteria
+          filter.removeColumnFilterCriteria(i);
+        } else {
+          originalCriteria.push(false);
+        }
+      }
+    }
+    // set new filter criteria
+    filterArray.forEach((item) => {
+      filter.setColumnFilterCriteria(columnLetterToNumber(item[0]), item[1].build());
+    });
+  }
+
+  // get hidden rows
+  let hiddenRowsIndexes = getHiddenRowsinGoogleSheets(app.getId(), sheet.getSheetId());
+
+  // reset to original filter criteria
+  if (setFilterFromScript) {
+    for (let i = 1; i <= lastColumn; i++) {
+      if (filter.getColumnFilterCriteria(i)) filter.removeColumnFilterCriteria(i);
+      if (originalCriteria[i - 1]) filter.setColumnFilterCriteria(i, originalCriteria[i - 1].build());
+    }
+  }
+
+  // get range values
+  let rangeValues = dataRange.getValues().filter((item, index) => !hiddenRowsIndexes.includes(index));
+
+  // get target spreadsheet
+  const targetApp = SpreadsheetApp.openById(localSpreadsheetId);
+
+  // get current time
+  const date = Utilities.formatDate(new Date(), 'GMT', 'yyyy/MM/dd HH:mm:ss');
+  // create new sheet
+  let targetSheet = targetApp.insertSheet();
+  // set time as a sheet name
+  targetSheet.setName(date);
+  // get target range
+  const targetRange = targetSheet.getRange(
+    ['A1', rangeNotation.split(':')[1].replace(lastRow, lastRow - hiddenRowsIndexes.length)].join(':')
+  );
+
+  // set target sheet values
+  targetRange.setValues(rangeValues);
+}
+
+// function to convert column letters to numbers
+function columnLetterToNumber(letter) {
+  let len = letter.length;
+  if (len === 1) {
+    return letter.charCodeAt(0) - 64;
+  } else if (len > 1) {
+    let letterNum = 0;
+    for (let i = 0; i < len; i++) {
+      let pow = len - 1 - i;
+      letterNum += (letter[i].charCodeAt(0) - 64) * 26 ** pow;
+    }
+    return letterNum;
+  }
+}
+
+// function to get hidden rows;
+const getHiddenRowsinGoogleSheets = (spreadsheetId, sheetId) => {
+  const fields = 'sheets(data(rowMetadata(hiddenByFilter)),properties/sheetId)';
+  const { sheets } = Sheets.Spreadsheets.get(spreadsheetId, { fields });
+  const [sheet] = sheets.filter(({ properties }) => {
+    return String(properties.sheetId) === String(sheetId);
+  });
+
+  const { data: [{ rowMetadata = [] }] = {} } = sheet;
+
+  const hiddenRows = rowMetadata
+    .map(({ hiddenByFilter }, index) => {
+      return hiddenByFilter ? index : -1;
+    })
+    .filter((rowId) => rowId !== -1);
+
+  return hiddenRows;
+};
